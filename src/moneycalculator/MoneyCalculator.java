da0e8484905bc49ba8329c80ca5cb570b67f40ba
sumit <sumit@LAPTOP-D2L81ON2>
2019-10-19 13:19:38 +0100

    MoneyCalculator version 2 (Codigo Modular modelo CIPO)

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -9,22 +9,44 @@ import java.util.Scanner;
 
 public class MoneyCalculator {
     
+    private double amount;
+    private double exchangeRate;
+    
+    private void input(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una cantidad en Dollares: ");
+        amount = Double.parseDouble(scanner.next());
+    }
+    
+    private void process() throws IOException{
+        this.exchangeRate = getExchangeRate("USD","EUR");
+    }
+    
+    private void output(){
+        System.out.println(this.amount + " USD equivalen a: " + amount*exchangeRate + "EUR");
+    }
+    
+    private void control() throws IOException {
+        input();
+        process();
+        output();
+    }
+    
     private static double getExchangeRate(String from, String to) throws IOException{
         URL url = new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" + from + "_" + to + "&compact=y&apiKey=1eb50c98f4a50de7d844");
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
         URLConnection connection = url.openConnection();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
             String line = reader.readLine();
             String line1 = line.substring(line.indexOf(to) + 12, line.indexOf("}"));
             return Double.parseDouble(line1);
+        
         }
     }
     
     public static void main(String[] args) throws IOException {
-        System.out.println("Introduzca una cantidad en Dollares: ");
-        Scanner scanner = new Scanner(System.in);
-        double amount = Double.parseDouble(scanner.next());
-        double exchangeRate = getExchangeRate("USD","EUR");
-        System.out.println(amount + " USD equivale a " + amount*exchangeRate + " EUR");
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
+        moneyCalculator.control();
     }
-    
+
 }
